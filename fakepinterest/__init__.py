8e232df5ebe459534b004a4a7cc81c624587502c
Bruno Rafaelle Lima Goes <BrunoGoes@odilonsantos.com>
2024-03-01 17:39:52 -0300

    psycog2 add

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///bancodados.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SECRET_KEY'] = "7c090c652a58bd6824d078823e78640e"
 app.config['UPLOAD_FOLDER'] = "static/fotos_post"
 
